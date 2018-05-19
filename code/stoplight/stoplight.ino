24d380b2286d872ff70c321d6ea3657a156710ba
carefudundae <levi.j.elston@gmail.com>
2018-05-18 20:50:59 -0400

    initial code working
    green light on until object in range, then red light for 5 sec, then yellow for 10 sec (switches back to red if object found again). currently 100ms loop time, will be decreased

--- code/stoplight/stoplight.ino
+++ code/stoplight/stoplight.ino
@@ -1,9 +1,109 @@
-void setup() {
-  // put your setup code here, to run once:
+int green_pin=7; 
+int yellow_pin=6;
+int red_pin=5;
+int relay_pin=4;
+int loop_delay=100; //milliseconds
+//int sonic_pin=8;
+int trig_pin=8;
+int echo_pin=9;
+int warning_time=5;  //seconds
+int caution_time=10; //seconds
 
+float distance;
+float min_distance=5;
+float max_distance=30;
+
+int alert=0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(green_pin, OUTPUT);
+  pinMode(yellow_pin, OUTPUT);
+  pinMode(red_pin, OUTPUT);
+  pinMode(trig_pin,OUTPUT);
+  pinMode(echo_pin,INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
+  distance=sonic_ping();
+
+  //check for someone/something in range
+  if (distance>min_distance && distance<max_distance)
+  {
+    red();
+    Serial.print("ALERT");
+    alert=1;
+    delay(warning_time*1000);    
+  }
+  else if (alert>0 && alert<(caution_time*(1000/loop_delay)))
+  {
+    yellow();
+    Serial.print("caution");
+    alert=alert+1;
+    //delay(loop_delay);
+    //delay(caution_time*1000); 
+  }
+  else
+  {
+    green();
+    Serial.print("clear");
+    alert=0;
+  }
+  //Serial.println();
+  Serial.print("   alert=");
+  Serial.print(alert);
+  Serial.println();
+  
+  delay(loop_delay);
+}
+int green(){
+  LED_on(green_pin);
+  LED_off(yellow_pin);
+  LED_off(red_pin);
+}
+int yellow(){
+  LED_off(green_pin);
+  LED_on(yellow_pin);
+  LED_off(red_pin);
+}
+int red(){
+  LED_off(green_pin);
+  LED_off(yellow_pin);
+  LED_on(red_pin);
+}
+int LED_on(int pin){
+  digitalWrite(pin,HIGH);
+}
+int LED_off(int pin){
+  digitalWrite(pin,LOW);
+}
+
+float sonic_ping(){
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+
+
+  digitalWrite(trig_pin,LOW); //turn off trig pin
+  delayMicroseconds(2); //short break
+  digitalWrite(trig_pin,HIGH);
+  delayMicroseconds(10); //pulse duration
+  digitalWrite(trig_pin,LOW);
+
+  float duration=pulseIn(echo_pin,HIGH);
+
+  // convert the time into a distance
+  // According to Parallax's datasheet for the PING))), there are 73.746
+  // microseconds per inch (i.e. sound travels at 1130 feet per second).
+  // This gives the distance travelled by the ping, outbound and return,
+  // so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  float dist = duration/74.0/2.0;
+  Serial.print(dist);
+  Serial.print(" in   ");
+  //Serial.println();
+  return dist;
+  
 }
